2ef1495ada315945865eca25121734ee4358b675
Leonardo Gonçalves <leogonsil97@gmail.com>
2020-10-10 17:22:36 -0300

    Using Agent

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,6 +1,15 @@
 defmodule ExMon do
-  alias ExMon.Player
+  alias ExMon.{Game, Player}
+
+  @computer_name "Robô"
+
   def create_payer(name, move_rnd, move_avg, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
+  end
+
+  def start_game(player) do
+    @computer_name
+    |> create_payer(:punch, :kick, :heal)
+    |> Game.start(player)
   end
 end
